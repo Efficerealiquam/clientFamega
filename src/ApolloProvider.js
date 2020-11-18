e2dd8604bd1c4918c527c539a429f4f7b34bbf55
Efficerealiquam <guillermo.flores.alcantara@gmail.com>
2020-11-18 07:41:06 -0500

    update URL API2

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://api.famega.pe",
+  uri: "http://api.famega.pe/",
 });
 
 const authLink = setContext(() => {
